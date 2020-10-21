f261184efd68311576ae5e5c16dede965f27ec89
Abid206 <61822329+Abid206@users.noreply.github.com>
2020-10-22 01:13:26 +0600

    added a file

---
+++ Indian Summer.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main()
+{
+    op();
+
+    int t;
+    cin>>t;
+
+    vector < pair<string, string> > v(t);
+    for(int i=0; i<t; i++)
+    {
+        cin>>v[i].first>>v[i].second;
+    }
+
+    sort(v.begin(), v.end());
+    int sz = unique(v.begin(), v.end())-v.begin();
+
+    cout<< sz<<endl;
+
+
+    return 0;
+}
+
+
